7a9bfdb20d721e1ca0e051beb89daeec4348c069
Umarhyall <umarbaharun941@gmail.com>
2023-05-02 18:54:50 +0700

    satoru

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-UMAR = 100;
+function showMembers(buttonIndex) {
+  var memberBoxes = document.querySelectorAll(".member-box");
+  for (var i = 0; i < memberBoxes.length; i++) {
+    memberBoxes[i].style.display = "none";
+  }
+
+  var startIndex = (buttonIndex - 1) * 12;
+  var endIndex = startIndex + 12;
+
+  for (var i = startIndex; i < endIndex; i++) {
+    if (memberBoxes[i]) {
+      memberBoxes[i].style.display = "block";
+    }
+  }
+}
